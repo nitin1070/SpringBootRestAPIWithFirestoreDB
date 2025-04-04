c3f7ce2ae6acd9a6915ca0153656bd2d2f092181
nitin1070 <nitin@alteredsecurity.com>
2025-04-04 11:26:00 +0530

    initial commit

--- src/main/java/in/main/exception/EmpAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package in.main.exception;
-
-public class EmpAlreadyExistException extends RuntimeException {
-	public EmpAlreadyExistException(String message) {
-		super(message);
-	}
-
-}
--- src/main/java/in/main/exception/NoSuchEmpExistException.java
+++
@@ -1,8 +0,0 @@
-package in.main.exception;
-
-public class NoSuchEmpExistException extends RuntimeException{
-	public NoSuchEmpExistException(String message) {
-		super(message);
-	}
-
-}
